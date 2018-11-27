cd4b6cc3d3a3f6225fde58c0eab41f8e46ad3e0d
Paul Abou Zeid <pabou049@uottawa.ca>
2018-11-26 22:45:02 -0500

    Updates

---
+++ D_FF.vhd
@@ -0,0 +1,33 @@
+--Define D_FF
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity D_FF is
+	
+	port (
+		clk: in std_logic;
+		rst: in std_logic;
+		d: in std_logic;
+		q: out std_logic
+	);
+
+end D_FF;
+
+architecture behv of D_FF is
+
+
+begin
+	process(clk, d)    --or (clk, rst)?
+		if rst = '0' then
+			q <= 0;
+		elsif(clk'event and clk = '1') then
+				q <= d;
+		end if;
+	end process;
+end behv;
+	
+
+
+
+	
--- shiftReg.vhd
+++ shiftReg.vhd
@@ -1,3 +1,15 @@
+--11 bit shift register with :
+--A start bit
+--Eight data bits (d0 to d7)
+--Two stop bits (sp1 and sp2)
+--The incoming data (din) connect to the sp2 flipflop and are shifted into the sp1 FF.
+--The last FF in the shift register is the start-bit FF, since this is the first data bit into the shift register.
+
+--Define the shift register
+--The shift register clock is rxclk which is controlled by the FSM
+--The protocol bits (st, sp1, and sp2) are shifted into theirs own FF's
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -11,3 +23,49 @@ entity shiftReg is
 	qst: out std_logic);
 
 end shiftReg;
+
+
+
+
+entity shiftReg is 
+
+	port (
+		rst: in std_logic;
+		clk: in std_logic;
+		din: in std_logic;
+		QST: out std_logic;
+		Q0: out std_logic;
+		Q1: out std_logic;
+		Q2: out std_logic;
+		Q3: out std_logic;
+		Q4: out std_logic;
+		Q5: out std_logic;
+		Q6: out std_logic;
+		Q7: out std_logic;
+		QSP1: out std_logic;
+		QSP2: out std_logic;
+		
+end shiftReg;
+
+
+architecture rtl of shiftReg is
+	signal dst, d0, d1, d2, d3, d4, d5, d6, d7, dsp1, dsp2 : std_logic;
+	begin
+
+	
+	
+	
+	
+	
+	dst <= Q0;
+	d0 = Q1;
+	d1 = Q2;
+	d2 = Q3;
+	d4 = Q5;
+	d5 = Q6;
+	d6 = Q7;
+	d7 = QSP1;
+	dsp1 = QSP2;
+	dsp2 = din;
+	end 
+	
