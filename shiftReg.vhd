f91d0cb5fec4181eb70a7bebcbbe9202c92cbccd
Ali Haidar <alihhaidar2@gmail.com>
2018-12-02 14:18:24 -0500

    port mapping started

--- shiftReg.vhd
+++ shiftReg.vhd
@@ -13,17 +13,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- entity shiftReg is
---
--- 	port (
--- 	q: out std_logic_vector(7 downto 0);
--- 	qsp1, qsp2: out std_logic;
--- 	clk, rx: in std_logic;
--- 	clr: in std_logic;
--- 	qst: out std_logic);
---
--- end shiftReg;
-
 entity shiftReg is 
 
 	port (
--- top_async2.vhd
+++ top_async2.vhd
@@ -32,11 +32,16 @@ architecture top_stru of top_async is
 	end component;
 	
 	component dataLatch
-		port ( oq: out std_logic_vector(7 downto 0);
-			d: in std_logic_vector(7 downto 0);
+		port ( OQ1, OQ2, OQ3, OQ4, OQ5, OQ6, OQ7: out std_logic;
+			D0, D1, D2, D3, D4, D5, D6, D7: in std_logic;
 			clk: in std_logic);
 	end component;
 	
+	begin
+		C0: fsm port map (SW(10), "rxf, rxo, ed, en, ack", CLOCK_50, KEY(0), CLR1, X1, "err, dry", PDCLK);
+		C1: shiftReg port map(CLR1, X1, "rx= DATA in", "QST", D0, D1, D2, D3, D4, D5, D6, D7, "QSP1, QSP2");
+		C2: divCounter port map("clr = CLR1"); -- dont have actual implementation so just saying what it should be
+		C3: dataLatch port map("oq", D0, D1, D2, D3, D4, D5, D6, D7, PDCLK);
 	
 	
 	
